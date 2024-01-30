4ea8773b47f6eb3878136dda1a898937c1013f91
huy034 <huylamdung@gmail.com>
2024-01-30 22:16:35 +0700

    Bai thuc hanh 24/1

---
+++ Baitapthuchanhjava/slide47.java
@@ -0,0 +1,18 @@
+package BTjava.Baitapthuchanhjava;
+
+import java.util.Scanner;
+
+public class slide47 {
+     public static void main(String[] args) {
+    int number, sum = 0;
+    Scanner sc = new Scanner(System.in);      
+    do {
+     System.out.println("Nhap vào so nguyen bat ky: ");
+       number = sc.nextInt();
+       sum += number;
+   } while (sum < 100); 
+     System.out.println("Tong cac so nguyen vua nhap = "+sum);
+     sc.close();
+    }
+  
+}
