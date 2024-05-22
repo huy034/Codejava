85bdfd972bc5d9f3165abd177e646c2556f2ac15
huy034 <huylamdung@gmail.com>
2024-05-22 16:19:47 +0700

    LDH388_Cau2

--- Baitapthuchanhjava/Baikiemtra/LDH388Person.java
+++ Baitapthuchanhjava/Baikiemtra/LDH388Person.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class LDH388Person {
     String ten;
     int tuoi;
     float ngaysinh;
-}
+
+    public void nhap (){
+    Scanner sc = new Scanner(System.in);
+  System.out.println("nhap thong tin ");
+    ten = sc.nextLine();
+    tuoi = sc.nextInt();
+    ngaysinh = sc.nextFloat();
+    }
+
+    public void xuatthongtin (){
+        System.out.println("Ten la " + ten);
+        System.out.println("Tuoi la " + tuoi);
+        System.out.println("ngay sinh la " + ngaysinh);
+    }
+    }
+
